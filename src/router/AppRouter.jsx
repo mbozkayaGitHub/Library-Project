883f8398cd2a665e94e0b4caeb534ba3b9168624
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-07-30 20:07:54 +0300

    Approuter

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./../components/navbar/Navbar";
+import Footer from "./../components/footer/Footer";
+import { GlobalStyle } from "../styles/Global.styles";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+import PrivateRouter from "./PrivateRouter";
+import About from "../pages/about/About";
+import Detail from "./../pages/detail/Detail";
+
+const AppRouter = () => {
+
+  const [currentUser, setCurrentUser] = useState(
+    sessionStorage.getItem("user")  || false 
+  )
+  return (
+    <BrowserRouter>
+      <Navbar  currentUser={currentUser} setCurrentUser={setCurrentUser}/>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setCurrentUser={setCurrentUser} />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="/detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route> */}
+        <Route element={<PrivateRouter />}>
+          <Route path="/about" element={<About />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Route>
+        
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
