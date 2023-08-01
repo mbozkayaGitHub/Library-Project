8b9dff84b626f20ae19b069513b9f627ac49e818
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-01 18:36:41 +0300

    Done2

---
+++ src/styles/Global.styles.jsx
@@ -0,0 +1,21 @@
+//create global styles
+
+//create global styles
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-transform: uppercase;
+        font-family: "Shantell Sans", cursive;
+
+    }
+
+    body {
+        font-size: 1.5rem;
+        background: ${({ theme }) => theme.colors.mainColor};
+    }
+`;
